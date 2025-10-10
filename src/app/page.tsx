ebc5b4cf4ad77f46003985d427e8c64c7a4ff71f
phuctran121 <phuc12104@gmail.com>
2025-10-10 14:38:17 +0700

    change image quality first load

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   return (
     <div className="relative w-full">
       <div className="relative w-full min-h-screen">
-        <div className="absolute inset-0 z-0">
+        <div className="absolute inset-0 z-0 bg-gradient-to-b from-card-3 to-white">
           <Image
             src="/assets/home/landing-bg.webp"
             alt="Landing background"
@@ -24,9 +24,8 @@ export default function HomePage() {
             priority
             sizes="100vw"
             className="object-cover object-top"
+            quality={70}
             decoding="async"
-            placeholder="blur"
-            blurDataURL="/assets/home/landing-bg-blur.webp"
             loading="eager"
           />
         </div>
