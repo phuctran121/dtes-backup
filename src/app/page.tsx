7caf0d71a7706c9c7dc26ed25e66d0e56b17d53f
phuctran121 <phuc12104@gmail.com>
2025-10-12 14:05:56 +0700

    recompltee page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,12 +16,8 @@ export default function HomePage() {
   return (
     <div className="relative w-full">
       <div className="relative w-full min-h-screen">
-        <h1 className="bg-red-400">ádasjasd</h1>
-        <h1 className="bg-red-400">ádasjasd</h1>
-        <h1 className="bg-red-400">ádasjasd</h1>
-        <h1 className="bg-red-400">ádasjasd</h1>
-        {/* <div className="absolute inset-0 z-0 bg-gradient-to-b from-card-3 to-white"> */}
-        {/* <Image
+        <div className="absolute inset-0 z-0">
+          <Image
             src="/assets/home/landing-bg.webp"
             alt="Landing background"
             fill
@@ -33,15 +29,15 @@ export default function HomePage() {
             priority={false}
             placeholder="blur"
             blurDataURL="/assets/home/landing-bg-blur.webp"
-          /> */}
-        {/* </div> */}
+          />
+        </div>
 
         <div className="relative z-10 flex flex-col">
-          {/* <HeroSection />
+          <HeroSection />
           <WhyDTESSection data={whyBoxesData} />
           <WhySection data={whyCardsData} />
           <InternationalStandardsSection data={iscCardsData} />
-          <SolutionsSection data={packagesCardsData} /> */}
+          <SolutionsSection data={packagesCardsData} />
         </div>
       </div>
     </div>
