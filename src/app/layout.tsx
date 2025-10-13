4e4902d2c2894ee5319f4a31c82d4bb71fb54146
phuctran121 <phuc12104@gmail.com>
2025-10-14 02:53:47 +0700

    finally reduce font size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,75 +3,32 @@ import "./globals.css";
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
 import localFont from "next/font/local";
-// import { pretendardFont } from "@croco/utils-next-font-pretendard";
 
-// const roboto = Roboto({
-//   subsets: ["latin"],
-//   weight: ["100", "300", "400", "500", "700", "900"],
-//   variable: "--font-roboto",
-//   fallback: ["system-ui", "arial"],
-// });
+const pretendard = localFont({
+  src: [
+    {
+      path: "../fonts/PretendardVariable.woff2",
+      style: "normal",
+      weight: "45 920",
+    },
+  ],
+  variable: "--font-pretendard",
+  display: "swap",
+  fallback: ["system-ui", "sans-serif"],
+  preload: true,
+});
+
 export const metadata: Metadata = {
-  title: "DTES - Design To Energy Saving",
+  title: "DTES",
   description:
     "DTES는 건축 설계 및 시공 전, 실시간 에너지 시뮬레이션을 통해 에너지 낭비를 줄이고 성능을 최적화할 수 있게 돕습니다.",
   keywords: ["디지털 트윈", "에너지 시뮬레이션", "건축 설계", "에너지 효율"],
-  authors: [{ name: "DTES" }],
-  creator: "DTES",
-  publisher: "DTES",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
+  openGraph: {
+    title: "DTES - Design To Energy Saving",
+    description: "건축 전에, 성능을 예측하세요",
+    // images: ["/assets/home/DTEs.jpg"],
   },
-  // metadataBase: new URL("https://dtes.kr"),
-  // openGraph: {
-  //   title: "DTES - Design To Energy Saving",
-  //   description: "건축 전에, 성능을 예측하세요",
-  //   type: "website",
-  //   locale: "ko_KR",
-  //   siteName: "DTES",
-  //   // images: ["/assets/home/DTEs.jpg"],
-  // },
-  // twitter: {
-  //   card: "summary_large_image",
-  //   title: "DTES - Design To Energy Saving",
-  //   description: "건축 전에, 성능을 예측하세요",
-  // },
-  // robots: {
-  //   index: true,
-  //   follow: true,
-  //   googleBot: {
-  //     index: true,
-  //     follow: true,
-  //     "max-video-preview": -1,
-  //     "max-image-preview": "large",
-  //     "max-snippet": -1,
-  //   },
-  // },
 };
-
-export const pretendard = localFont({
-  src: [
-    {
-      path: "../fonts/Pretendard-Regular.woff2",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Pretendard-Medium.woff2",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Pretendard-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  display: "swap",
-  variable: "--font-pretendard",
-});
 
 export default function RootLayout({
   children,
@@ -79,8 +36,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="kr">
-      <body className={`${pretendard.className}  antialiased `}>
+    <html
+      lang="kr"
+      className={`${pretendard.variable}`}
+      suppressHydrationWarning
+    >
+      <body
+        suppressHydrationWarning
+        className={`${pretendard.className} antialiased`}
+      >
         <div className="relative z-10">
           <Navigation />
           <div className="min-h-screen bg-transparent">
